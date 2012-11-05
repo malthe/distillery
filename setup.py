08256a1cdaab0d46f971773ab15570bee17e2782
jean-philippe serafin <serafinjp@gmail.com>
2012-11-05 13:08:23 +0100

    Bumps 0.3.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='distillery',
-    version='0.3.1',
+    version='0.3.2',
     author=u'jean-philippe serafin',
     author_email='serafinjp@gmail.com',
     py_modules=('distillery',),
